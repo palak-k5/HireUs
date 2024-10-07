aaca576af2f260edf9883d2b2d686ea74927633d
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-10-07 23:10:57 +0530

    initial configuration of backend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+// const express= require('express') 
+//this is old method of including express but we have used the new one that is mentioning type as module in package,json file
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app=express();
+app.get("/home",(req,res)=>{
+    return res.status(200).json({
+        message:"I am coming from backend",
+        succes:true
+    })
+}); 
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
+const corsOption={
+    origin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOption)); 
+
+const PORT=3000;
+app.listen (PORT,()=>{
+    console.log(`Server running  at port ${PORT }`)
+})
