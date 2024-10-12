2992175b0de2b6963f2e69c04b0e6a9a5e1de1c8
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-10-13 01:43:33 +0530

    Set up backend and connectivity with mongodb along with job,company model description

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app=express();
 app.get("/home",(req,res)=>{
@@ -24,7 +27,8 @@ const corsOption={
 }
 app.use(cors(corsOption)); 
 
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 app.listen (PORT,()=>{
+    connectDB();
     console.log(`Server running  at port ${PORT }`)
 })
