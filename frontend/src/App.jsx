221a5d6b5af0f6237788ac0169a4a9788f589d25
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-11-11 11:30:38 +0530

    added navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/shared/navbar'
+
+function App() {
+ 
+  return (
+    <>
+    <h1 className="text-teal-600">Welcome to HireUs </h1>
+    <Navbar></Navbar>
+          </>
+  )
+}
+
+export default App
