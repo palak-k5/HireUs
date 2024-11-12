971adaeb839eea375cf446a541ddd32c71447476
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-11-12 15:41:50 +0530

    index.js ,json file and gitignore added

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,40 @@
-// const express= require('express') 
-//this is old method of including express but we have used the new one that is mentioning type as module in package,json file
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
 dotenv.config({});
 
-const app=express();
-app.get("/home",(req,res)=>{
-    return res.status(200).json({
-        message:"I am coming from backend",
-        succes:true
-    })
-}); 
+const app = express();
 
-//middleware
+// middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-
-const corsOption={
-    origin:'http//localhost:5173',
+const corsOptions = {
+    origin:'http://localhost:5173',
     credentials:true
 }
-app.use(cors(corsOption)); 
 
-const PORT=process.env.PORT || 3000;
-app.listen (PORT,()=>{
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+
+
+app.listen(PORT,()=>{
     connectDB();
-    console.log(`Server running  at port ${PORT }`)
+    console.log(`Server running at port ${PORT}`);
 })
