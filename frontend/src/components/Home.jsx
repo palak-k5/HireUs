534ab4ff607184340c82d40e7c873d38f5406966
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-11-17 23:50:15 +0530

    herosection added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Navbar from './shared/navbar'
-
+import HeroSection from './HeroSection'
 const Home = () => {
   return (
+    <div>
     <Navbar/>
+    <HeroSection/>
+    {// <CategoryCarousel/>
+    // <LatestJobs/>
+    // <Footer/>
+    }
+    </div>
+    
   )
 }
 
