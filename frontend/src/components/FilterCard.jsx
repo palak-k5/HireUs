14ab2dadb60dd5408d85d5daa03b708d894698ae
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-11-23 20:03:10 +0530

    filtercard and category carousel added

---
+++ frontend/src/components/FilterCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { RadioGroup, RadioGroupItem } from './ui/radio-group'
+import { Label } from './ui/label'
+
+const filterData = [
+    {
+        filterType: "Location",
+        array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+    },
+    {
+        filterType: "Industry",
+        array: ["Frontend Developer", "Backend Developer", "FullStack Developer"]
+    },
+    {
+        filterType: "Salary",
+        array: ["0-40k", "42-1lakh", "1lakh to 5lakh"]
+    },
+]
+
+const FilterCard = () => {
+    return (
+        <div className='w-full bg-white p-3 rounded-md text-dark-blue'>
+            <h1 className='font-bold text-lg'>Filter Jobs</h1>
+            <hr className='mt-1 ' />
+            <RadioGroup>
+  {filterData.map((data, index) => (
+    <div key={index}>
+      <h1 className="font-bold text-lg">{data.filterType}</h1>
+      {data.array.map((item, idx) => (
+        <div className="flex items-center space-x-2 my-2" key={idx}>
+          <RadioGroupItem value={item} />
+          <Label>{item}</Label>
+        </div>
+      ))}
+    </div>
+  ))}
+</RadioGroup>
+
+        </div>
+    )
+}
+
+export default FilterCard
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from './shared/navbar'
 import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
+import LatestJob from './LatestJob'
+import Footer from './shared/Footer'
 const Home = () => {
   return (
     <div>
     <Navbar/>
     <HeroSection/>
-    {// <CategoryCarousel/>
-    // <LatestJobs/>
-    // <Footer/>
-    }
+    <CategoryCarousel/>
+    <LatestJob/>
+    <Footer/>
+    
     </div>
     
   )
