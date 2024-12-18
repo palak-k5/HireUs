18fc6c456c13e6321a2b54c3ffea7702f171c0c0
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-12-18 23:26:52 +0530

    Revert "changes for production"
    This reverts commit ae4366c3e2ef63744409b82ef058d0d7efbddc96.

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,11 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import path from "path";
-import exp from "constants";
+
 dotenv.config({});
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const _dirname=path.resolve();
 
 // middleware
 app.use(express.json());
@@ -35,10 +33,7 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
-app.get('*', (_,res) =>{
-    res.sendFile(path.resolve(_dirname,"frontend","dist", "index.html"));
-});
+
 
 app.listen(PORT,()=>{
     connectDB();
