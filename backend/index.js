70e859fcab47a2e1b30611205f76d96de81ef10a
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-12-19 23:24:54 +0530

    changes for production after one attempt

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,13 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import path from "path";
 dotenv.config({});
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+const _dirname=path.resolve();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -32,9 +33,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res) =>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
