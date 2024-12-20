073f9d446c692551ea2675ec1c31794d1f1f01dc
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-12-20 12:18:30 +0530

    navbar updated

--- frontend/src/components/admin/Companies.jsx
+++ frontend/src/components/admin/Companies.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from '../shared/navbar.jsx';
+import Navbar from '../shared/Navbar.jsx';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import CompaniesTable from './CompaniesTable';
