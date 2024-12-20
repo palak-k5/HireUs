157ca16e3dc9cdd3013523d271cd8b16750fe23b
PALAK KUMRAWAT <palakkumrawat05@gmail.com>
2024-12-20 12:39:23 +0530

    navbar in home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Navbar from './shared/Navbar';
+import Navbar from '../components/shared/Navbar';
 import HeroSection from './HeroSection';
 import CategoryCarousel from './CategoryCarousel';
 import LatestJob from './LatestJob';
